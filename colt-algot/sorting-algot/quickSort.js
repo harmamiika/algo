017e8cb9e31e979b50ebe3fa942b5504236224c8
Miika Härmä <harmamiika@gmail.com>
2022-06-12 10:14:49 +0300

    qyicksort end

--- colt-algot/sorting-algot/quickSort.js
+++ colt-algot/sorting-algot/quickSort.js
@@ -1,4 +1,4 @@
-function pivot(array, startIndex = 0, endIndex = array.length + 1) {
+function pivot(array, startIndex = 0, endIndex = array.length - 1) {
   function swap(i1, i2) {
     const temp = array[i1];
     array[i1] = array[i2];
@@ -21,3 +21,18 @@ function pivot(array, startIndex = 0, endIndex = array.length + 1) {
 }
 
 // function quickSort(array, left = )
+
+function quickSort(array, left = 0, right = array.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(array, left, right);
+    // left
+    quickSort(array, left, pivotIndex - 1);
+    // right
+    quickSort(array, pivotIndex + 1, right);
+  }
+  return array;
+}
+
+// jos pivot on min tai max - algo n^2, muuten normi ja best case n logn
+
+console.log(quickSort([4, 7, 69, 100, 1, 420, 42, 999, 11]), 'quick sort');
