c7c121cabd1b819748f9ca8deaa6f0a7706517b3
Miika Härmä <harmamiika@gmail.com>
2022-07-01 18:04:41 +0300

    graphtraversal

--- graph.js
+++ graph.js
@@ -39,4 +39,99 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  dfsRecursive(vertex) {
+    const result = [];
+    const visited = {};
+    // Huom
+    const adjacencyList = this.adjacencyList;
+
+    function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      for (let adjacentVertex of adjacencyList[vertex]) {
+        if (!visited[adjacentVertex]) {
+          dfs(adjacentVertex);
+        }
+      }
+    }
+    dfs(vertex);
+
+    return result;
+  }
+
+  dfsIterative(start) {
+    const S = [];
+    const result = [];
+    const visited = {};
+    let vertex;
+
+    S.push(start);
+    visited[start] = true;
+
+    while (S.length > 0) {
+      vertex = S.pop();
+      result.push(vertex);
+
+      this.adjacencyList[vertex].forEach((adjacent) => {
+        if (!visited[adjacent]) {
+          visited[adjacent] = true;
+          S.push(adjacent);
+        }
+      });
+    }
+
+    return result;
+  }
+
+  bfs(start) {
+    const Q = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+
+    while (Q.length) {
+      currentVertex = Q.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((adjacent) => {
+        if (!visited[adjacent]) {
+          visited[adjacent] = true;
+          Q.push(adjacent);
+        }
+      });
+    }
+
+    return result;
+  }
 }
+
+let g = new Graph();
+
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
+
+console.log(g.dfsRecursive('A'));
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
