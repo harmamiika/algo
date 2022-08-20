e0910d656d7379f4535f8d7667c9ce33339f52a2
Miika Härmä <harmamiika@gmail.com>
2022-08-20 12:55:53 +0300

    watercontaioneer

--- leetcodeTop75/9ContainerWIthMostWater.js
+++ leetcodeTop75/9ContainerWIthMostWater.js
@@ -1,1 +1,26 @@
-var maxArea = function (height) {};
+var maxArea = function (height) {
+  let max = 0,
+    l = 0,
+    r = height.length - 1;
+
+  while (l < r) {
+    const area = Math.min(height[l], height[r]) * (r - l);
+    max = Math.max(area, max);
+    if (height[l] < height[r]) {
+      l++;
+    } else r--;
+  }
+
+  return max;
+};
+
+// var maxArea = function (height) {
+//     let max = 0;
+//     for (let i = 0; i < height.length; i++) {
+//       for (let j = i + 1; j < height.length; j++) {
+//         const area = Math.min(height[i], height[j]) * j - i;
+//         if (area > max) max = area;
+//       }
+//     }
+//     return max;
+//   };
