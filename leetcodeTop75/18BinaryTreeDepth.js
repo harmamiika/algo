c1d16b6d97f52b96de62dcc27437e5c6e8a54d84
Miika Härmä <harmamiika@gmail.com>
2022-08-26 20:58:55 +0300

    maxdepth2

--- leetcodeTop75/18BinaryTreeDepth.js
+++ leetcodeTop75/18BinaryTreeDepth.js
@@ -10,3 +10,8 @@ var maxDepth = function (root) {
   }
   return Math.max(traverse(root.left, 1), traverse(root.right, 1));
 };
+
+var maxDepth = function (root) {
+  if (!root) return 0;
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
