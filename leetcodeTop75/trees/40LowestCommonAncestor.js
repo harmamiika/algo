1b3b8d0c93db9fe6bd101987944a5f3306b40891
Miika Härmä <harmamiika@gmail.com>
2022-10-01 21:15:52 +0300

    40lowestcommonancestor

--- leetcodeTop75/trees/40LowestCommonAncestor.js
+++ leetcodeTop75/trees/40LowestCommonAncestor.js
@@ -1,5 +1,12 @@
-var lowestCommonAncestor = function(root, p, q) {
-    // check if even possible
-    if (root.val > )
-    if (root.val > q && root.val > p) return false
+// O(logn)
+
+// IN BST
+var lowestCommonAncestor = function (root, p, q) {
+  let curr = root;
+  while (curr) {
+    if (p.val > curr.val && q.val > curr.val) curr = curr.right;
+    else if (p.val < curr.val && q.val < curr.val) curr = curr.left;
+    // IF SPLIT => always LCA
+    else return curr;
+  }
 };
