8bfb55f4484c6dc1782630b4fdf6b9632e1bc00c
Miika Härmä <harmamiika@gmail.com>
2022-10-11 19:19:36 +0300

    course schedule dogshit?

---
+++ leetcodeTop75/graph/48CourseSchedule.js
@@ -0,0 +1,39 @@
+// <==
+var canFinish = function (numCourses, prerequisites) {
+  const graph = createGraph(numCourses, prerequisites);
+  let seen = new Set();
+  let seeing = new Set();
+
+  for (let i = 0; i < numCourses; i++) {
+    if (!explore(i)) return false;
+  }
+  return true;
+
+  function explore(course) {
+    if (seen.has(course)) return true;
+    if (seeing.has(course)) return false;
+
+    seeing.add(course);
+    for (let neighbor of graph[course]) {
+      if (!explore(neighbor)) return false;
+    }
+
+    seen.add(course);
+    seeing.delete(course);
+    return true;
+  }
+};
+
+function createGraph(numCourses, edges) {
+  const graph = new Array(numCourses.length).fill([]);
+  for (let edge of edges) {
+    let [a, b] = edge;
+    // a lyhyempi kuin indeksi???
+    console.log(a, b, ' = edge');
+    if (!(a in graph)) graph[a] = [];
+    if (!(b in graph)) graph[b] = [];
+
+    graph[a].push(b);
+  }
+  return graph;
+}
